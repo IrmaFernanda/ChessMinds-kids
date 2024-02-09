6e17bae1552c0221e682dedb862db0b278c8ca00
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2024-02-09 09:45:31 -0500

    :construction: (CHESS-12) Agregado electronjs

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+
+const url = require("url");
+
+const path = require("path");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Electron",
+    width: 1000,
+    height: 600,
+  });
+
+  // mainWindow.webContents.openDevTools();
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "./app/build/index.html"),
+    protocol: "file",
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createMainWindow);
