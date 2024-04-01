88ae11916cf356d37f885fe9352cb939ad943e53
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2024-03-31 19:08:44 -0500

    :bug: (RECH25) Fix back button

--- src/renderer/src/components/Button/BackButton.tsx
+++ src/renderer/src/components/Button/BackButton.tsx
@@ -1,13 +1,14 @@
 import { FaArrowLeft } from 'react-icons/fa'
 import { ActionButton, ActionButtonProps } from './ActionButton'
-import { Link } from 'react-router-dom'
 
 export const BackButton = ({ className, ...props }: ActionButtonProps) => {
+  const handleClick = () => {
+    window.history.back()
+  }
+
   return (
-    <Link to={-1}>
-      <ActionButton className={className} {...props}>
-        <FaArrowLeft className="w-4 h-4 text-gray-600" />
-      </ActionButton>
-    </Link>
+    <ActionButton className={className} onClick={handleClick} {...props}>
+      <FaArrowLeft className="w-4 h-4 text-gray-600" />
+    </ActionButton>
   )
 }
