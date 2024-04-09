494e982ba82810cd99befad578af635f47ea7006
Fernanda <fernandacar@unicauca.edu.co>
2024-04-09 17:27:25 -0500

    Arreglar imagenes y ejercicios de descubierta y atrapada

--- src/renderer/src/components/Board/Square.tsx
+++ src/renderer/src/components/Board/Square.tsx
@@ -4,6 +4,7 @@ import { twMerge } from 'tailwind-merge'
 type SquareProps = { black: boolean } & ComponentProps<'div'>
 
 export const Square = ({ black, children }: SquareProps) => {
-  const bgClass = black ? 'bg-[#d9f99d]' : 'bg-[#047857]'
+  const bgClass = black ? 'bg-[#dcfce7]' : 'bg-[#14532d]'
+  //blanco: green-100 negras: esmeral-900
   return <div className={twMerge('w-full h-full', bgClass)}>{children}</div>
 }
--- src/renderer/src/components/Button/BackButton.tsx
+++ src/renderer/src/components/Button/BackButton.tsx
@@ -8,7 +8,7 @@ export const BackButton = ({ className, ...props }: ActionButtonProps) => {
 
   return (
     <ActionButton className={className} onClick={handleClick} {...props}>
-      <FaArrowLeft className="w-7 h-7 text-gray-600" />
+      <FaArrowLeft className="w-7 h-7 text-gray-800" />
     </ActionButton>
   )
 }
